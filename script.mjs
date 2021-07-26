01c0df1a6730a612e2192eb6ff62b2b69338d5d9
Domenic Denicola <d@domenic.me>
2021-07-26 18:52:34 -0400

    Overhaul and reorganize the algorithm
    It should no longer infinite loop or mess up on the last line sometimes.

--- script.mjs
+++ script.mjs
@@ -5,56 +5,78 @@ const columnLength = document.querySelector("#column-length");
 input.addEventListener("input", updateOutput);
 columnLength.addEventListener("input", updateOutput);
 
+// To catch reloads if the algorithm has changed during development:
+updateOutput();
+
 function updateOutput() {
   output.value = rewrap(input.value, columnLength.valueAsNumber);
 }
 
 function rewrap(text, columnLength) {
   const lines = text.split("\n");
-  const linesSmushed = [];
+  const unwrapped = unwrapNonStandaloneLines(lines);
+  const rewrapped = wrapLines(unwrapped, columnLength);
+  return rewrapped.join("\n");
+}
+
+function unwrapNonStandaloneLines(lines) {
+  const unwrappedLines = [];
 
   let lastLineIsSmushable = false;
   for (const line of lines) {
-    if (isOnlyATag(line) || isOnlyWhitespace(line)) {
-      linesSmushed.push(line);
+    if (shouldStandalone(line)) {
+      unwrappedLines.push(line);
       lastLineIsSmushable = false;
     } else {
       if (lastLineIsSmushable) {
-        linesSmushed[linesSmushed.length - 1] += " " + line.trim();
+        unwrappedLines[unwrappedLines.length - 1] += " " + line.trim();
       } else {
-        linesSmushed.push(line.trimRight());
+        unwrappedLines.push(line.trimRight());
       }
       lastLineIsSmushable = true;
     }
   }
 
+  return unwrappedLines;
+}
+
+function wrapLines(lines, columnLength) {
   const linesRewrapped = [];
-  for (const line of linesSmushed) {
+  for (const line of lines) {
     if (line.length <= columnLength) {
       linesRewrapped.push(line);
     } else {
-      let leftToRewrap = line.trim();
-      const leadingIndent = getLeadingIndent(line);
-      const charsPerLine = columnLength - leadingIndent.length;
-
-      while (leftToRewrap.length > columnLength) {
-        const spaceToSplitOnIndex = leftToRewrap.lastIndexOf(" ", charsPerLine);
-        linesRewrapped.push(leadingIndent + leftToRewrap.substring(0, spaceToSplitOnIndex));
-        leftToRewrap = leftToRewrap.substring(spaceToSplitOnIndex + 1);
-      }
-      linesRewrapped.push(leadingIndent + leftToRewrap);
+      linesRewrapped.push(...wrapLine(line, columnLength));
     }
   }
 
-  return linesRewrapped.join("\n");
+  return linesRewrapped;
 }
 
-function isOnlyATag(line) {
-  return line.match(/^\s*<\/?[a-z-]+>\s*$/i);
+function wrapLine(line, columnLength) {
+  const leadingIndent = getLeadingIndent(line, columnLength);
+
+  const brokenLines = [];
+  let currentLine = "";
+  let spaceBefore = "";
+  for (const word of line.split(" ")) {
+    if (currentLine.length + spaceBefore.length + word.length < columnLength) {
+      currentLine += spaceBefore + word;
+    } else {
+      brokenLines.push(currentLine);
+      currentLine = leadingIndent + word;
+    }
+    spaceBefore = " ";
+  }
+  brokenLines.push(currentLine);
+
+  return brokenLines;
 }
 
-function isOnlyWhitespace(line) {
-  return line.match(/^\s*$/);
+function shouldStandalone(line) {
+  const trimmed = line.trim();
+
+  return trimmed.length === 0 || /^<\/?[a-z-]+>$/i.test(trimmed) || /^<!--.*?-->$/i.test(trimmed);
 }
 
 function getLeadingIndent(line) {
